687f9f93c8a232760db47c23b0df8e798b351a99
Corey Leigh Latislaw <colabug@gmail.com>
2013-06-04 15:53:05 -0400

    Added gradient text, cleaned up styling

--- src/com/colabug/chiuki/BeautifulAndroidActivity.java
+++ src/com/colabug/chiuki/BeautifulAndroidActivity.java
@@ -1,9 +1,13 @@
 package com.colabug.chiuki;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class BeautifulAndroidActivity extends Activity
 {
@@ -18,7 +22,27 @@ public class BeautifulAndroidActivity extends Activity
 
         // Set typeface on button
         Button button = (Button) findViewById( R.id.button );
-        Typeface typeface = Typeface.createFromAsset( getAssets(), "Chunkfive.otf" );
-        button.setTypeface( typeface );
+        button.setTypeface( getTypefaceFromAsset() );
+
+        // Text gradient
+        TextView gradientTextView = (TextView) findViewById( R.id.gradient_text );
+        gradientTextView.getPaint()
+                        .setShader( getLinearGradient( gradientTextView ) );
+    }
+
+    private Typeface getTypefaceFromAsset()
+    {
+        return Typeface.createFromAsset( getAssets(), "Chunkfive.otf" );
+    }
+
+    private LinearGradient getLinearGradient( TextView textView )
+    {
+        return new LinearGradient( 0,
+                                   0,
+                                   0,
+                                   textView.getTextSize(),
+                                   Color.RED,
+                                   Color.BLUE,
+                                   Shader.TileMode.CLAMP );
     }
 }
