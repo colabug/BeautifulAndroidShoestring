2d063d4c607c3a796ec835dad3e7d4912f79df7b
Corey Leigh Latislaw <colabug@gmail.com>
2013-06-07 14:23:53 -0400

    Styling: More styling for icon page
    * Added clicking to the icons

--- src/com/colabug/chiuki/BeautifulIconsActivity.java
+++ src/com/colabug/chiuki/BeautifulIconsActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class BeautifulIconsActivity extends Activity
 {
@@ -11,6 +14,38 @@ public class BeautifulIconsActivity extends Activity
     {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.icons );
+        setUpIconClickListeners();
+    }
+
+    private void setUpIconClickListeners()
+    {
+        View android = findViewById( R.id.android_icon );
+        android.setOnClickListener( createIconOnClickListener() );
+
+        View camera = findViewById( R.id.camera_icon );
+        camera.setOnClickListener( createIconOnClickListener() );
+
+        View music = findViewById( R.id.music_icon );
+        music.setOnClickListener( createIconOnClickListener() );
+
+        View leaf = findViewById( R.id.leaf_icon );
+        leaf.setOnClickListener( createIconOnClickListener() );
+    }
+
+    private View.OnClickListener createIconOnClickListener()
+    {
+        return new View.OnClickListener()
+        {
+            @Override
+            public void onClick( View view )
+            {
+                TextView textView = (TextView) view;
+                String toastText = "Icon font text: " + textView.getText();
+                Toast.makeText( BeautifulIconsActivity.this,
+                                toastText,
+                                Toast.LENGTH_SHORT ).show();
+            }
+        };
     }
 
     public static Intent createIntent( Context context )
